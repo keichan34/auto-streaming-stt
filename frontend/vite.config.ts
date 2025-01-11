d05233375471c920de1f47119983123f9b5c38d5
Keita Kobayashi <keita@kbys.me>
2025-01-11 17:19:25 +0900

    Updates

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig, loadEnv } from 'vite'
+import { visualizer } from "rollup-plugin-visualizer";
 import react from '@vitejs/plugin-react'
 import { VitePWA } from 'vite-plugin-pwa'
 
@@ -43,6 +44,10 @@ export default defineConfig(({mode}) => {
           enabled: true,
           type: 'module',
         }
+      }),
+      visualizer({
+        filename: 'stats.html',
+        gzipSize: true,
       }),
     ],
     server: {
