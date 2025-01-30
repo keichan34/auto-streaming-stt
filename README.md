1bb8f983e8cdde2b2d4a1ccbd354a3f3432235c6
Keita Kobayashi <keita@kbys.me>
2025-01-30 12:05:25 +0900

    Update header/footer, README

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 # auto-streaming-stt
 
-Raspberry Pi appliance to automatically stream audio to a speech-to-text service, with a live-updating browser user interface.
+Raspberry Pi appliance to automatically stream audio to a speech-to-text service, use OpenAI APIs to summarize the content, and send notifications to clients using the [Web Notification API](https://developer.mozilla.org/en-US/docs/Web/API/Notification).
+
+このプロジェクトは Raspberry Pi 上で実行することを前提とし、入力音声から自動文字起こしサービスにストリーミングし、 OpenAI API を使って要約し、 Web Notification API を使って通知を送信します。
+
+鹿児島県屋久島町の防災放送に繋いだサンプルアプリケーションはこちらで確認できます: [https://bousai.yakushima.blog/](https://bousai.yakushima.blog/)
+
+開発のきっかけ、流れなどに興味ある方はぜひ、下記のブログを参照してください。
+
+* [防災放送をデジタル化してみた](https://keita.blog/2023/03/26/%E4%B9%85%E3%81%97%E3%81%B6%E3%82%8A%E3%81%AB%E5%9C%B0%E5%9B%B3%E3%81%A8%E9%96%A2%E4%BF%82%E3%81%AA%E3%81%84%E3%82%82%E3%81%AE%E3%82%92%E4%BD%9C%E3%82%8A%E3%81%BE%E3%81%97%E3%81%9F/)
+* [防災放送をデジタル化してみた: 続き](https://keita.blog/2023/10/15/%E9%98%B2%E7%81%BD%E6%94%BE%E9%80%81%E3%82%92%E3%83%87%E3%82%B8%E3%82%BF%E3%83%AB%E5%8C%96%E3%81%97%E3%81%A6%E3%81%BF%E3%81%9F-%E7%B6%9A%E3%81%8D/)
 
 ## Installation
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -105,24 +105,33 @@ function App() {
   }, [listMutate, mutate, reconnect]);
 
   return (
-    <div className="container">
-      <h1 className="my-4">屋久島の防災放送</h1>
+    <div className="min-vh-100 d-flex flex-column">
+      <header className="bg-primary-subtle text-center py-3">
+        <h1 className="fs-4">屋久島の防災放送</h1>
+      </header>
 
-      { !isRunningStandalone && (
-        <p>
-          このサイトをホーム画面に追加すると、通知を受け取ることができます。
-        </p>
-      )}
+      <main className="flex-fill container">
+        { !isRunningStandalone && (
+          <p>
+            このサイトをホーム画面に追加すると、通知を受け取ることができます。
+          </p>
+        )}
 
-      { ('Notification' in window) && (!isSubscribed && (
-        <div className="d-grid gap-2">
-          <button type="button" className="btn btn-primary mb-4" onClick={subscribeHandler}>
-            通知を受け取る
-          </button>
-        </div>
-      )) }
+        { ('Notification' in window) && (!isSubscribed && (
+          <div className="d-grid gap-2">
+            <button type="button" className="btn btn-primary mb-4" onClick={subscribeHandler}>
+              通知を受け取る
+            </button>
+          </div>
+        )) }
 
-      <TranscriptionListView />
+        <TranscriptionListView />
+      </main>
+
+      <footer className="bg-dark text-white text-center py-3">
+        <p>このアプリは非公式のもの・一切の責任を負いません</p>
+        <p><a className="text-light" href="https://github.com/keichan34/auto-streaming-stt" rel="noopener noreferrer" target="_blank">詳細はこちら</a>・<a className="text-light" href="mailto:keita+yakubousai@kbys.me?subject=屋久島防災放送について" rel="noopener noreferrer" target="_blank">連絡はこちら</a></p>
+      </footer>
     </div>
   );
 }
